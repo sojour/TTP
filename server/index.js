186ac4908fbde502d23528dc45db7cdada3d2f9b
Wei Ji <monokuroai@gmail.com>
2019-09-30 20:11:19 -0400

    fix-app and server now works locally

--- server/index.js
+++ server/index.js
@@ -3,28 +3,30 @@ const path = require('path')
 const morgan = require('morgan')
 
 const app = express();
-const PORT = process.env.PORT || 8080;
 
 app.use(morgan('dev'));
 
-app.use(express.json({ limit: '50mb', extended: true }))
-app.use(express.urlencoded({ limit: '50mb', extended: true }))
+app.use(express.json({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../build')));
 
-app.use((req, res, next) => {
-  if (path.extname(req.path).length) {
-    const err = new Error('Not found')
-    err.status = 404
-    next(err)
-  } else {
-    next()
-  }
-})
+
+// app.use((req, res, next) => {
+//   if (path.extname(req.path).length) {
+//     const err = new Error('Not found')
+//     err.status = 404
+//     next(err)
+//   } else {
+//     next()
+//   }
+// })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
+
 
 app.use(function (err, req, res, next) {
   console.error(err);
@@ -32,6 +34,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500).send(err.message || 'Internal server error.');
 });
 
-app.listen(PORT, () =>
-  console.log(`Starting server on ${PORT}`)
-)
+module.exports = app
+
