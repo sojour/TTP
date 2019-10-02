60106862684cad063f8733b5ca33ab4d11d780ba
Wei Ji <monokuroai@gmail.com>
2019-10-02 15:17:04 -0400

    feat-impleted store with redux

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+// import user from './users';
+import user from './user'
+import shares from './shares';
+
+
+const reducer = combineReducers({ user, shares });
+const middleware = composeWithDevTools(
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
+
+export default store;
