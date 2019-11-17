d5ab1e5bb4a4647f6614a6337c94f3f588d34716
Wei Ji <monokuroai@gmail.com>
2019-11-17 17:22:35 -0500

    feat-implemented dd tracing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const db = require('./server/db')
 const app = require('./server')
 const PORT = process.env.PORT || 8080;
-
+const tracer = require('dd-trace').init()
 
 app.listen(PORT, async () =>
   await db.sync(),
